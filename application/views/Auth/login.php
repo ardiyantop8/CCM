9825155b9ca364d071edad9400eb4bb0ee42e68b
ardiputra04 <ardidesti04@gmail.com>
2021-02-13 16:22:40 +0700

    membuat style css untuk login

---
+++ application/views/Auth/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <title>Form Login</title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <div class="kotak_login">
+        <p class="tulisan_login">Silahkan login</p>
+
+        <form class="isi-form">
+            <label>Username</label>
+            <input type="text" name="username" class="form_login" placeholder="Username atau email ..">
+
+            <label>Password</label>
+            <input type="text" name="password" class="form_login" placeholder="Password ..">
+
+            <input type="submit" class="tombol_login" value="LOGIN">
+
+            <br />
+            <br />
+            <p class="kembali">
+                <a class="link" href="#">kembali</a>
+            </p>
+        </form>
+
+    </div>
+</body>
+
+</html>
