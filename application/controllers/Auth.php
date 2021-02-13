6fcc03b578794f9e71bedb6f6d94d25b4e257b14
ardiputra04 <ardidesti04@gmail.com>
2021-02-13 19:38:31 +0700

    Menambahkan assets bootstrap

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,10 +1,86 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Auth extends CI_Controller {
+class Auth extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
 
 	public function index()
 	{
-		$this->load->view('Auth/login');
+		if ($this->session->userdata('username')) {
+			redirect('user');
+		}
+
+		$this->form_validation->set_rules('username', 'Username', 'trim|required');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+		if ($this->form_validation->run() == false) {
+			$data['title'] = 'Form Login';
+			$this->load->view('Auth/login', $data);
+			// $this->load->view('template/auth-header', $data_title);
+			// $this->load->view('auth/login');
+			// $this->load->view('template/auth-footer');
+		} else {
+			//validasinya lolos
+			$this->_login();
+		}
+		// $this->load->view('Auth/login');
+	}
+
+	private function _login()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+
+		$user = $this->db->get_where('user', ['username' => $username])->row_array();
+
+		//usernya ada
+		if ($user) {
+			//jika usernya aktif
+			if ($user['is_active'] == 1) {
+				//cek passwordnya
+				if (password_verify($password, $user['password'])) {
+					$data = [
+						'username' 		=> $user['username'],
+						'role_id'		=> $user['role_id']
+					];
+					$this->session->set_userdata($data);
+					if ($user['role_id'] == 1) {
+						redirect('Admin');
+					} else if ($user['role_id'] == 2) {
+						redirect('Owner');
+					} else {
+						redirect('Kasir');
+					}
+				} else {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Password Salah ! </div>');
+					redirect('Auth');
+				}
+			} else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Username tidak aktif, Silahkan Hubungi Developer </div>');
+				redirect('Auth');
+			}
+		} else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Username belum terdaftar </div>');
+			redirect('Auth');
+		}
+	}
+
+	public function logout()
+	{
+		$this->session->unset_userdata('username');
+		$this->session->unset_userdata('role_id');
+
+		$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Anda berhasil keluar </div>');
+		redirect('Auth');
+	}
+
+	public function blocked()
+	{
+		$this->load->view('Auth/blocked');
 	}
 }
--- application/views/Auth/login.php
+++ application/views/Auth/login.php
@@ -5,34 +5,69 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url("assets/style.css");  ?>">
+    <!-- <link rel="stylesheet" href="<?= base_url("assets/style.css");  ?>"> -->
     <title>Form Login</title>
+    <!-- Custom fonts for this template-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url("assets/vendor/fontawesome-free/css/all.min.css");  ?>">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link rel="stylesheet" href="<?= base_url("assets/css/sb-admin-2.min.css");  ?>">
+    <link rel="shortcut icon" href="<?= base_url('assets/img/icon/vote2.png'); ?>">
     <style>
 
     </style>
 </head>
 
 <body>
-    <div class="kotak_login">
-        <p class="tulisan_login">Silahkan login</p>
+    <div class="container">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Form Login</h1>
+                                        <?= $this->session->flashdata('message');  ?>
+                                    </div>
+                                    <form class="user" method="post" action="<?= base_url('Auth');  ?>">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Masukan Username..." value="<?= set_value('username');  ?>">
+                                            <?= form_error('username', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" name="password" id="password" placeholder="Password">
+                                            <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </div>
 
-        <form class="isi-form">
-            <label>Username</label>
-            <input type="text" name="username" class="form_login" placeholder="Username atau email ..">
-
-            <label>Password</label>
-            <input type="text" name="password" class="form_login" placeholder="Password ..">
-
-            <input type="submit" class="tombol_login" value="LOGIN">
-
-            <br />
-            <br />
-            <p class="kembali">
-                <a class="link" href="#">kembali</a>
-            </p>
-        </form>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                        <hr>
+                                        <marquee>Aplikasi Sistem Company Cash Management !!</marquee>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="#">Lupa Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <!-- <a class="small" href="<?= base_url('umum');  ?>">Kembali</a> -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
+
+
 </body>
 
 </html>
